f64ef199ed598af7340770ef6e3d70be644d2019
yogeshwar <ppatilyogeshwar507@gmail.com>
2020-04-06 04:12:36 +0000

    imlemented calculator

---
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -0,0 +1,41 @@
+package com.ravi.cal.RaviCalculator;
+
+import java.util.Scanner;
+
+public class Calculator  {
+	
+	private long first;
+	private long second;
+	
+	public long getFirst() {
+		return first;
+	}
+
+	public void setFirst(long first) {
+		this.first = first;
+	}
+
+	public long getSecond() {
+		return second;
+	}
+
+	public void setSecond(long second) {
+		this.second = second;
+	}
+
+	public long addFucn(long first, long second){
+		
+		return first+second;
+	}
+	
+	public long subFucn(long first, long second){
+		
+		return second-first;
+	}
+	
+	public long mulFucn(long first, long second){
+		
+		return first/second;
+	}
+}
+
